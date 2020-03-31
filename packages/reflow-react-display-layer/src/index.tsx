02f8399b8501d8affd9f642ee491a3a5e597816a
Ido Ben-Harosh <ido.b@mce-sys.com>
2020-03-31 17:43:39 +0300

    exporting ReflowDisplayLayerElement to allow embedded/testing usage

--- packages/reflow-react-display-layer/src/index.tsx
+++ packages/reflow-react-display-layer/src/index.tsx
@@ -1,6 +1,7 @@
 import { ReflowTransport, ViewsMapInterface } from "@mcesystems/reflow";
 import { render } from "react-dom";
 import * as React from "react";
+import { useState, useEffect } from "react";
 import ReflowDisplayLayer, { ViewsComponents } from "./ReflowDisplayLayer";
 
 export * from "./ReflowReactComponent";
@@ -12,14 +13,35 @@ export interface ReactReflowOptions<ViewMap extends ViewsMapInterface, ViewerPar
 	wrapper?: React.ComponentClass<ViewerParameters>;
 }
 
-export function renderDisplayLayer<ViewMap extends ViewsMapInterface, ViewerParameters = {}>({ transport, element, views, wrapper }: ReactReflowOptions<ViewMap, ViewerParameters>) {
+export interface ReactReflowDisplayLayerElementProps<ViewMap extends ViewsMapInterface, ViewerParameters = {}> {
+	transport: ReflowTransport<ViewerParameters>;
+	views: ViewsComponents<ViewMap>;
+	wrapper?: React.ComponentClass<ViewerParameters>;
+}
+
+export const ReflowDisplayLayerElement = <ViewMap extends ViewsMapInterface, ViewerParameters = {}>({
+	views,
+	transport,
+	wrapper
+}: ReactReflowDisplayLayerElementProps<ViewMap, ViewerParameters>) => {
 	const Wrapper = (wrapper || React.Fragment) as typeof wrapper;
+	const [viewerParams, setViewerParams] = useState({} as ViewerParameters);
 
-	transport.onViewerParameters((viewerParams) => {
-		render(<Wrapper {...viewerParams}>
+	useEffect(() => {
+		transport.onViewerParameters((params) => {
+			setViewerParams(params);
+		});
+
+		transport.initializeAsDisplay();
+	}, [transport]);
+
+	return (
+		<Wrapper {...viewerParams}>
 			<ReflowDisplayLayer transport={transport} views={views} />
-		</Wrapper>, element);
-	});
+		</Wrapper>
+	);
+}
 
-	transport.initializeAsDisplay();
+export function renderDisplayLayer<ViewMap extends ViewsMapInterface, ViewerParameters = {}>({ transport, element, views, wrapper }: ReactReflowOptions<ViewMap, ViewerParameters>) {
+	render(<ReflowDisplayLayerElement views={views} transport={transport} wrapper={wrapper} />, element);
 }
