15cc43ca02eaa9ed11e319c4156bf073a5fa1de4
shmuelhizmi <shmuelhizmi1@gmail.com>
2020-07-19 23:52:41 +0300

    support for socketTransport paths

--- examples/phone-book-app/server/index.ts
+++ examples/phone-book-app/server/index.ts
@@ -1,11 +1,32 @@
 import { Transports, Reflow } from "@mcesystems/reflow";
 import { ViewInterfacesType, viewInterfaces } from "../viewInterfaces";
 import mainFlow from "./flows/main";
+import { createServer } from 'http'
 
-const transport = new Transports.WebSocketsTransport({ port: 12345 });
+const server = createServer();
+server.listen(12345);
+
+const transport = new Transports.WebSocketsTransport({}, server);
+const transportSecond = new Transports.WebSocketsTransport({ path: "/second" }, server);
+const transportStandalone = new Transports.WebSocketsTransport({ port: 12346 });
 
 const reflow = new Reflow<ViewInterfacesType>({
 	transport,
 	views: viewInterfaces,
 });
+
 reflow.start(mainFlow)
+
+const reflowSecond = new Reflow<ViewInterfacesType>({
+	transport: transportSecond,
+	views: viewInterfaces,
+});
+
+reflowSecond.start(mainFlow)
+
+const reflowStandalone = new Reflow<ViewInterfacesType>({
+	transport: transportStandalone,
+	views: viewInterfaces,
+});
+
+reflowStandalone.start(mainFlow)
