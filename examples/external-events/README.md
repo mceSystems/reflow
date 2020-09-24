c4784cb159db8dd62086e990a70eaff58a028748
AdiIzr <34090715+AdiIzr@users.noreply.github.com>
2020-09-24 15:30:00 +0300

    add ExternalEvent function to flow (#34)
    * add ExternalEvent function to flow
    
    * add ExternalEvent function to flow
    
    * add external event to flow toolkit
    
    Co-authored-by: Adi Izraeli <adi.i@tlv.mce-sys.com>

---
+++ examples/external-events/README.md
@@ -0,0 +1,8 @@
+# external-events
+This is a flow-only example, without any views or display layer. It will show how to use the `externalEvent()` function in order to send exteranl events out of flow and how to use reflow `addEventListener()` and `removeEventListener()` in order to listen to the fired events.
+
+## Running
+`npm install` \
+and \
+`npm run build`\
+Then run `node index`
---
+++ examples/external-events/index.ts
@@ -0,0 +1,50 @@
+import { Transports, Reflow, Flow } from "@mcesystems/reflow";
+
+interface ExternalEvents {
+	myExternalEvent: {
+		data: string;
+	};
+	otherExternalEvent: {
+		data: string;
+	}
+}
+
+const mainFlow = <Flow<{}, void, void, {}, {}, {}, ExternalEvents>>(async ({externalEvent}) => {
+	// this should trigger a console.log
+	externalEvent("myExternalEvent", {
+		data: "myExternalEventData"
+	});
+	// this should trigger 2 console.log
+	externalEvent("otherExternalEvent", {
+		data: "otherExternalEvent"
+	})
+	// this should not trigger a console.log
+	externalEvent("otherExternalEvent", {
+		data: "otherExternalEvent"
+	})
+});
+
+const reflow = new Reflow<{}>({
+	transport: new Transports.InProcTransport({}),
+	views: {},
+});
+
+const l1 = ({ data }) => {
+	console.log(data); //"myExternalEventData"
+	reflow.removeEventListener("myExternalEvent", l1);
+}
+
+const l2 = ({ data }) => {
+	console.log(data); //"otherExternalEvent", "data"
+	reflow.removeEventListener("otherExternalEvent");
+}
+
+const l3 = ({ data }) => {
+	console.log(data); //"otherExternalEvent", "data"
+}
+
+reflow.addEventListener("myExternalEvent", l1);
+reflow.addEventListener("otherExternalEvent", l2);
+reflow.addEventListener("otherExternalEvent", l3);
+
+reflow.start(mainFlow);
